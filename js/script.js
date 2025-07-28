2d1ee710a4a43051774078afced1fce4dacf8bcf
joker <gdhjyt68vhbe85.h@gmail.com>
2025-07-28 23:40:19 +0500

    Ура, массажка

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let button = document.getElementById("myButton");
+let text = document.getElementById("zhopa");
+button.addEventListener('click', suda)
+function suda() {
+    alert("От души братишка");
+    text.innerHTML = "<h1>Когда в массажку?)</h1>"
+};
