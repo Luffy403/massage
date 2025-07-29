8f259a2cf75eff592e14416ca7b0148981750d46
joker <gdhjyt68vhbe85.h@gmail.com>
2025-07-29 12:36:40 +0500

    Ура, обнова

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 let button = document.getElementById("myButton");
 let text = document.getElementById("zhopa");
-button.addEventListener('click', suda)
+button.addEventListener('click', suda);
 function suda() {
     alert("От души братишка");
-    text.innerHTML = "<h1>Когда в массажку?)</h1>"
+    text.innerHTML =  "<div class = 'zagolov'><h1>Когда в массажку?)</h1>  <button id='secondBut'>Погнали!</button>  </div>";
+    let secondBut = document.getElementById("secondBut");
+    let socialIcon = document.querySelector(".container");
+    secondBut.addEventListener('click', function secondFun(){
+        socialIcon.innerHTML = "<h2>Запись на массаж:</h2>" 
+        + "<a href = 'https://t.me/bekreev_massagennov' target = 'blank'><img src = 'icon/Telegram.svg'></img></a>"
+    });
 };
